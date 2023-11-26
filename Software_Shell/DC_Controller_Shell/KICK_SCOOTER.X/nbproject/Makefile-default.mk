dc81e04107adb67b50f129c4c00fe11506a3322d
HugoBrunner <napapplebeats23@gmail.com>
2023-11-26 16:14:06 +0100

    Comit

--- Software_Shell/DC_Controller_Shell/KICK_SCOOTER.X/nbproject/Makefile-default.mk
+++ Software_Shell/DC_Controller_Shell/KICK_SCOOTER.X/nbproject/Makefile-default.mk
@@ -30,18 +30,24 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 IMAGE_TYPE=debug
 OUTPUT_SUFFIX=elf
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=${DISTDIR}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 else
 IMAGE_TYPE=production
 OUTPUT_SUFFIX=hex
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=${DISTDIR}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 endif
 
 ifeq ($(COMPARE_BUILD), true)
 COMPARISON_BUILD=-mafrlcsj
 else
 COMPARISON_BUILD=
+endif
+
+ifdef SUB_IMAGE_ADDRESS
+SUB_IMAGE_ADDRESS_COMMAND=--image-address $(SUB_IMAGE_ADDRESS)
+else
+SUB_IMAGE_ADDRESS_COMMAND=
 endif
 
 # Object Directory
@@ -64,7 +70,6 @@ OBJECTFILES=${OBJECTDIR}/_ext/410712389/Main.o ${OBJECTDIR}/_ext/410712389/X2CSc
 SOURCEFILES=../Firmware/Srs/Source/Main.c ../Firmware/Srs/Source/X2CScope.c ../Firmware/Srs/Source/X2CScopeComm.c ../Firmware/Srs/Source/init_dspic.c ../Firmware/Srs/Source/uart1.c
 
 
-
 CFLAGS=
 ASFLAGS=
 LDLIBSOPTIONS=
@@ -82,73 +87,83 @@ FIXDEPS=fixDeps
 ifneq ($(INFORMATION_MESSAGE), )
 	@echo $(INFORMATION_MESSAGE)
 endif
-	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
 MP_PROCESSOR_OPTION=33CK32MP105
 MP_LINKER_FILE_OPTION=,--script=p33CK32MP105.gld
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/410712389/Main.o: ../Firmware/Srs/Source/Main.c  .generated_files/flags/default/956fcf4cb4fe0d482a7f712f094dd7f5f43527c3 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/Main.o: ../Firmware/Srs/Source/Main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/Main.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/Main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/Main.c  -o ${OBJECTDIR}/_ext/410712389/Main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/Main.o.d"      -g -D__DEBUG   -mno-eds-warn  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/Main.c  -o ${OBJECTDIR}/_ext/410712389/Main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/Main.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/Main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/X2CScope.o: ../Firmware/Srs/Source/X2CScope.c  .generated_files/flags/default/47cbaaf0deb9a672700297db4649cdbb0dfc15c8 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/X2CScope.o: ../Firmware/Srs/Source/X2CScope.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScope.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScope.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScope.c  -o ${OBJECTDIR}/_ext/410712389/X2CScope.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScope.o.d"      -g -D__DEBUG   -mno-eds-warn  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScope.c  -o ${OBJECTDIR}/_ext/410712389/X2CScope.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScope.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/X2CScope.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/X2CScopeComm.o: ../Firmware/Srs/Source/X2CScopeComm.c  .generated_files/flags/default/676b59d4413cc6c014ce89f530556cf986613c6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/X2CScopeComm.o: ../Firmware/Srs/Source/X2CScopeComm.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScopeComm.c  -o ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d"      -g -D__DEBUG   -mno-eds-warn  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScopeComm.c  -o ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/init_dspic.o: ../Firmware/Srs/Source/init_dspic.c  .generated_files/flags/default/728574192efe8ed36ea58316cb627b9bba198c13 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/init_dspic.o: ../Firmware/Srs/Source/init_dspic.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/init_dspic.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/init_dspic.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/init_dspic.c  -o ${OBJECTDIR}/_ext/410712389/init_dspic.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/init_dspic.o.d"      -g -D__DEBUG   -mno-eds-warn  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/init_dspic.c  -o ${OBJECTDIR}/_ext/410712389/init_dspic.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/init_dspic.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/init_dspic.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/uart1.o: ../Firmware/Srs/Source/uart1.c  .generated_files/flags/default/aafd2c4adcaccc7ca3fdf2db69682661fae87435 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/uart1.o: ../Firmware/Srs/Source/uart1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/uart1.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/uart1.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/uart1.c  -o ${OBJECTDIR}/_ext/410712389/uart1.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/uart1.o.d"      -g -D__DEBUG   -mno-eds-warn  -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/uart1.c  -o ${OBJECTDIR}/_ext/410712389/uart1.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/uart1.o.d"      -g -D__DEBUG   -mno-eds-warn  -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/uart1.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 else
-${OBJECTDIR}/_ext/410712389/Main.o: ../Firmware/Srs/Source/Main.c  .generated_files/flags/default/288b664ab728c5fc9fb9d8d21da8767c4e68a06d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/Main.o: ../Firmware/Srs/Source/Main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/Main.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/Main.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/Main.c  -o ${OBJECTDIR}/_ext/410712389/Main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/Main.o.d"      -mno-eds-warn  -g -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/Main.c  -o ${OBJECTDIR}/_ext/410712389/Main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/Main.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/Main.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/X2CScope.o: ../Firmware/Srs/Source/X2CScope.c  .generated_files/flags/default/5f732ed098eedd46f6cf5e03f514112a164d541b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/X2CScope.o: ../Firmware/Srs/Source/X2CScope.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScope.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScope.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScope.c  -o ${OBJECTDIR}/_ext/410712389/X2CScope.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScope.o.d"      -mno-eds-warn  -g -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScope.c  -o ${OBJECTDIR}/_ext/410712389/X2CScope.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScope.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/X2CScope.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/X2CScopeComm.o: ../Firmware/Srs/Source/X2CScopeComm.c  .generated_files/flags/default/6b7d3cbe34525d6965283916fd254e459c5d874f .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/X2CScopeComm.o: ../Firmware/Srs/Source/X2CScopeComm.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScopeComm.c  -o ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d"      -mno-eds-warn  -g -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/X2CScopeComm.c  -o ${OBJECTDIR}/_ext/410712389/X2CScopeComm.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/X2CScopeComm.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/init_dspic.o: ../Firmware/Srs/Source/init_dspic.c  .generated_files/flags/default/20d85130684a1d6557dc074c66b060d642b4d67a .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/init_dspic.o: ../Firmware/Srs/Source/init_dspic.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/init_dspic.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/init_dspic.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/init_dspic.c  -o ${OBJECTDIR}/_ext/410712389/init_dspic.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/init_dspic.o.d"      -mno-eds-warn  -g -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/init_dspic.c  -o ${OBJECTDIR}/_ext/410712389/init_dspic.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/init_dspic.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/init_dspic.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
-${OBJECTDIR}/_ext/410712389/uart1.o: ../Firmware/Srs/Source/uart1.c  .generated_files/flags/default/e46913d1e07f3424a33894e6aba00357d73d8c4d .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/_ext/410712389/uart1.o: ../Firmware/Srs/Source/uart1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}/_ext/410712389" 
 	@${RM} ${OBJECTDIR}/_ext/410712389/uart1.o.d 
 	@${RM} ${OBJECTDIR}/_ext/410712389/uart1.o 
-	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/uart1.c  -o ${OBJECTDIR}/_ext/410712389/uart1.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/_ext/410712389/uart1.o.d"      -mno-eds-warn  -g -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off   
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ../Firmware/Srs/Source/uart1.c  -o ${OBJECTDIR}/_ext/410712389/uart1.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/_ext/410712389/uart1.o.d"      -mno-eds-warn  -g -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -O1 -I"../Firmware/Srs/Source" -msmart-io=1 -Wall -msfr-warn=off  
+	@${FIXDEPS} "${OBJECTDIR}/_ext/410712389/uart1.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
@@ -164,3 +179,38 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 else
 endif
 
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../Firmware/Srs/Lib/libx2cscope-33ck-elf.a ../Firmware/Srs/Lib/Kick_scooter.X.a  
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    ..\Firmware\Srs\Lib\libx2cscope-33ck-elf.a ..\Firmware\Srs\Lib\Kick_scooter.X.a  -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG=__DEBUG   -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include"     -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D__DEBUG=__DEBUG,,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--library="q",--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST) 
+	
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  ../Firmware/Srs/Lib/libx2cscope-33ck-elf.a ../Firmware/Srs/Lib/Kick_scooter.X.a 
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    ..\Firmware\Srs\Lib\libx2cscope-33ck-elf.a ..\Firmware\Srs\Lib\Kick_scooter.X.a  -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -I"../Firmware/Srs/Include" -Wl,--local-stack,,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--library="q",--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml$(MP_EXTRA_LD_POST) 
+	${MP_CC_DIR}\\xc16-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/KICK_SCOOTER.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf  
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/default
+	${RM} -r dist/default
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
